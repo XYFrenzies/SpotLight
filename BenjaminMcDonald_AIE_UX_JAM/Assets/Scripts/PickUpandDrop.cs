37effc9461bfbdf9035e7aefeeb09bc5ab4a72d5
Ben <bennybill16@gmail.com>
2021-04-09 16:00:55 +1000

    Adding a sideproject to work on called spotlight

---
+++ BenjaminMcDonald_AIE_UX_JAM/Assets/Scripts/PickUpandDrop.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpandDrop : MonoBehaviour
+{
+    [SerializeField] private Transform m_dest;
+    private void OnMouseDown()
+    {
+        GetComponent<BoxCollider>().enabled = false;
+        GetComponent<Rigidbody>().useGravity = false;
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+        this.transform.position = m_dest.position;
+        this.transform.parent = GameObject.Find("PickUp Pos").transform;
+    }
+    private void OnMouseUp()
+    {
+        this.transform.parent = null;
+        GetComponent<BoxCollider>().enabled = true;
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+    }
+}
---
+++ BenjaminMcDonald_AIE_UX_JAM/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb = null;
+    [SerializeField] private float speed = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.velocity.magnitude > speed)
+        {
+            rb.velocity = Vector3.ClampMagnitude(rb.velocity, speed);
+        }
+        
+        float moveHori = Input.GetAxis("Horizontal");
+        float moveVert = Input.GetAxis("Vertical");
+        Vector3 movement = transform.right * moveHori + transform.forward * moveVert;
+        if(movement.magnitude >= 0.1f)
+            rb.AddForce(movement * speed);
+    }
+}
